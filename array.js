dc1a339e8ad9f65210e6ff3e3b8adbc79dba09e4
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-08-02 15:08:52 +0600

    Problem 2 solved

--- array.js
+++ array.js
@@ -1,8 +1,21 @@
 // Problem 1 (Declare an Array)
-const fruits = ["Mango", "Lichi", "Banana", "Apple", "Pineapple"];
+// const fruits = ["Mango", "Lichi", "Banana", "Apple", "Pineapple"];
 
-console.log(fruits[3]);
+// console.log(fruits[3]);
 
-fruits[2] = "jambura";
+// fruits[2] = "jambura";
 
-console.log(fruits);
+// console.log(fruits);
+
+
+// Problem 2 (Add or remove elements)
+
+const tourist = ["Cox's Bazar", "Jaflong", "Shat Gombuj Mosque"];
+
+tourist.push("Sreemangal");
+
+tourist.push("Ratargul", "Sundarban");
+
+tourist.pop();
+
+console.log(tourist);
