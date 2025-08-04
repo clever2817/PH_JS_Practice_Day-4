6d38008f54779336573263dc10a94e6763295200
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-08-04 19:31:58 +0600

    Problem 4 solved

--- array.js
+++ array.js
@@ -34,3 +34,54 @@ else
 {
     console.log("Javascript book not available.");
 }
+
+
+// Problem 4 (Checking if it's an array)
+
+const places = ["Dhaka", "Khulna", "Rajshahi", "Barisal"];
+
+const age = 28;
+
+const fullName = "Menhajul Abir";
+
+isConsistent = true;
+
+if(Array.isArray(places))
+{
+    console.log("This variable is an array.");
+}
+
+else
+{
+    console.log("This variable is not an array.");
+}
+
+if(Array.isArray(age))
+{
+    console.log("This variable is an array.");
+}
+
+else
+{
+    console.log("This variable is not an array.");
+}
+
+if(Array.isArray(fullName))
+{
+    console.log("This variable is an array.");
+}
+
+else
+{
+    console.log("This variable is not an array.");
+}
+
+if(Array.isArray(isConsistent))
+{
+    console.log("This variable is an array.");
+}
+
+else
+{
+    console.log("This variable is not an array.");
+}
